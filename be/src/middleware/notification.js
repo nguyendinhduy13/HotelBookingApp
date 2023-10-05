641103fe8a5b05551c6310024cb97c00d8bd7659
DuyChamHocAC <duy.tran@apps-cyclone.com>
2023-10-05 23:27:46 +0700

    notification

---
+++ be/src/middleware/notification.js
@@ -0,0 +1,37 @@
+const FCM = require("fcm-node");
+const serverKey = process.env.serverKeyNotification;
+const fcm = new FCM(serverKey);
+
+const sendNotification = (req, res) => {
+  const { token, title, data, body } = req.body;
+  try {
+    const message = {
+      to: token,
+      notification: {
+        title: title,
+        body: body,
+      },
+      data: data,
+    };
+    fcm.send(message, function (err, response) {
+      if (err) {
+        console.log("Something has gone wrong!");
+        return res.status(400).send({
+          message: "Something has gone wrong!",
+        });
+      } else {
+        console.log("Successfully sent with response: ", response);
+        res.status(200).send({
+          message: "Successfully sent with response: ",
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({
+      message: "Something has gone wrong!",
+    });
+  }
+};
+
+module.exports = sendNotification;
