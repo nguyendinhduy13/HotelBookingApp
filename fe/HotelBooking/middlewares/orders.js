fcbe53e1a7bd91534999b5654554fa425533230f
Quang Vu <ngqangvvu@gmail.com>
2023-11-01 03:09:14 +0700

    Update middlewares

--- fe/HotelBooking/middlewares/orders.js
+++ fe/HotelBooking/middlewares/orders.js
@@ -16,28 +16,21 @@ export const GetOrders = async id => {
   }
 };
 
-
-export const UpdateReview = async id => {
-  const API = `${LOCAL_API_URL}/orders/reviewd/${id}`;
+export const GetOrder = async id => {
+  const API = `${LOCAL_API_URL}/orders/${id}`;
   try {
-    const response = await fetch(API, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({}),
-    });
+    const response = await fetch(API);
     const data = await response.json();
     return {status: 200, data: data};
   } catch (error) {
     return {
       status: 401,
-      message:
-        'An error occurred while updating review. Please try again later.',
+      message: 'An error occurred while getting order. Please try again later.',
       error: error,
     };
   }
 };
+
 
 export const AddNewOrder = async dataOrder => {
   const API = `${LOCAL_API_URL}/orders/addneworder`;
