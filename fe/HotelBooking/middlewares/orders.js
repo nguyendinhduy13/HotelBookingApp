1ca3449299300134ab2a27ed679f19bb37f9e862
Quang Vu <ngqangvvu@gmail.com>
2023-11-04 03:09:57 +0700

    Fix bugs for middlewares

--- fe/HotelBooking/middlewares/orders.js
+++ fe/HotelBooking/middlewares/orders.js
@@ -31,6 +31,27 @@ export const GetOrder = async id => {
   }
 };
 
+export const UpdateReview = async id => {
+  const API = `${LOCAL_API_URL}/orders/reviewd/${id}`;
+  try {
+    const response = await fetch(API, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    });
+    const data = await response.json();
+    return {status: 200, data: data};
+  } catch (error) {
+    return {
+      status: 401,
+      message:
+        'An error occurred while updating review. Please try again later.',
+      error: error,
+    };
+  }
+};
 
 export const AddNewOrder = async dataOrder => {
   const API = `${LOCAL_API_URL}/orders/addneworder`;
@@ -77,24 +98,3 @@ export const AddNewOrder = async dataOrder => {
   }
 };
 
-export const UpdateStatus = async (id, status) => {
-  const API = `${LOCAL_API_URL}/orders/status/${id}`;
-  try {
-    const response = await fetch(API, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({status}),
-    });
-    const data = await response.json();
-    return {status: 200, data: data};
-  } catch (error) {
-    return {
-      status: 401,
-      message:
-        'An error occurred while updating status. Please try again later.',
-      error: error,
-    };
-  }
-};
