8dfb1972f26b7f37f424f1265650a31ce6b5f0ef
Quang Vu <ngqangvvu@gmail.com>
2023-11-10 04:03:03 +0700

    Update notifications

--- fe/HotelBooking/src/components/Notification.js
+++ fe/HotelBooking/src/components/Notification.js
@@ -24,7 +24,63 @@ const Notification = () => {
         zIndex: 100,
       }}>
       {notification?.length > 0 && (
-        
+        <Pressable
+          onPress={() => {
+            console.log('aaa');
+          }}
+          style={{
+            width: '95%',
+            backgroundColor: 'white',
+            borderRadius: 10,
+            alignSelf: 'center',
+            elevation: 5,
+          }}>
+          <View style={{padding: 10}}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <Image
+                  source={require('../assets/logo_mini.png')}
+                  style={{
+                    width: 25,
+                    height: 25,
+                    resizeMode: 'cover',
+                  }}
+                />
+              </View>
+              <Text
+                style={{
+                  fontSize: 15,
+                  fontWeight: 'bold',
+                  color: 'black',
+                  marginLeft: 10,
+                }}>
+                {notification[0]?.title}
+              </Text>
+            </View>
+            <View style={{marginTop: 5}}>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 13,
+                  fontWeight: '400',
+                  lineHeight: 20,
+                  textAlign: 'justify',
+                  overflow: 'hidden',
+                  height: notification[0]?.body.length > 110 ? 40 : null,
+                }}>
+                {formatLongText(notification[0]?.body)}
+              </Text>
+            </View>
+          </View>
+        </Pressable>
       )}
     </View>
   );
